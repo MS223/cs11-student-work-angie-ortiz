f301215b225b1c7c6fe74cf5d2551c35acf72fa5
angie-ortiz <angie.ortiz@ms223.org>
2016-03-28 08:23:40 -0400

    Update Fizzbuzz.py

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,4 +1,10 @@
-for groot in range(1,101):
-    if groot % 3 == 0:
+for groot in range(1,101): #this will give me numbers from 1-100
+    if groot % 3 == 0 + groot % 5==0: #this makes the computer check for multiples of 3 and 5
+        print "fizzbuzz" #it will replace the multiple with "fizzbuzz"
+    elif groot % 5 == 0: #it took me a while to realize that I needed an elif
+        print "buzz"
+    elif groot % 3 ==0: #this checks for a multiple of 3 only
         print "fizz"
-    elif
+    else:
+        print groot #this calls the function
+
