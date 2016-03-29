bbe7ffc6bc7bdb44ed5c86869be96801125bd0f7
angie-ortiz <angie.ortiz@ms223.org>
2016-03-29 08:37:51 -0400

    Update CountdownV2.py

--- CountdownV2.py
+++ CountdownV2.py
@@ -1,5 +1,5 @@
-starting_time= input("Enter number:")
-while starting_time>0 in range(0,starting_time):
+starting_time= input("Enter number:") #I kept this part the same
+while starting_time>0 in range(0,starting_time): #The range is the same as the first countdown assignment, I just added the while loop
     print starting_time
-    starting_time=starting_time-1
+    starting_time=starting_time-1 #I want the numbers to countdown so I need to subtract 1 each time
 print "Boom!"
