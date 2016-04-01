eb6b9fce3a1006ed12ed9d84cf372a29893d5030
angie-ortiz <angie.ortiz@ms223.org>
2016-04-01 08:42:20 -0400

    Create GuessingGame.py

---
+++ GuessingGame.py
@@ -0,0 +1,7 @@
+user=raw_input("What is your name?")
+user=raw_input("What do you want the highest possible number to be?")
+guess=(1,10)
+guess.randint(1,10)
+print guess
+
+#look more into random numbers with python on google.
