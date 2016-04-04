a553439a5e6cdabecff47d82a1cc8421fa3181d5
angie-ortiz <angie.ortiz@ms223.org>
2016-04-04 08:41:45 -0400

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,7 +1,9 @@
 user=raw_input("What is your name?")
-user=raw_input("What do you want the highest possible number to be?")
-guess=(1,10)
-guess.randint(1,10)
-print guess
-
-#look more into random numbers with python on google.
+highest_number=input("Give me the highest number you can think of:")
+import random
+answer=random.randint(0,highest_number)
+guess= input("What's your guess?")
+print answer
+# 
+if guess > #Finish the feedback given to the user
+    print "Not quite"
