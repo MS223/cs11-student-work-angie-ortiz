4e84e91a95f09c9d43684c9a8cf9d3afe538e42e
angie-ortiz <angie.ortiz@ms223.org>
2016-04-05 08:47:42 -0400

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,3 +1,4 @@
+guesses=0
 user=raw_input("What is your name?")
 highest_number=input("What's the highest number you would like to choose from:")
 import random
@@ -8,9 +9,11 @@ while guess != answer:
     if guess > answer:
         print "Too high"
         guess= input("What's your guess?")
+        guesses= guesses+1
     else:
-        # guess < answer; this is unnecessary because the else part already knows that if it's not too high, then it's automatically too low.
+        # guess < answer; this is unnecesarry because the else part already knows that if it's not too high, then it's automatically too low.
         print "Too low"
         guess= input("What's your guess?")
+        guesses= guesses+1
 print "Your guess was correct!"
-
+print "It took you" + str(guesses) + "tries"
