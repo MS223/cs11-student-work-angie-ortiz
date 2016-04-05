3182c2393ecdbe706c83cb50520e88b95b5bb51e
angie-ortiz <angie.ortiz@ms223.org>
2016-04-05 08:20:56 -0400

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,9 +1,16 @@
 user=raw_input("What is your name?")
-highest_number=input("Give me the highest number you can think of:")
+highest_number=input("What's the highest number you would like to choose from:")
 import random
 answer=random.randint(0,highest_number)
 guess= input("What's your guess?")
-print answer
-# 
-if guess > #Finish the feedback given to the user
-    print "Not quite"
+# print answer; this would give away the answer to the user
+while guess != answer:
+    if guess > answer:
+        print "Too high"
+        guess= input("What's your guess?")
+    else:
+        # guess < answer; this is unnecessary because the else part already knows that if it's not too high, then it's automatically too low.
+        print "Too low"
+        guess= input("What's your guess?")
+print "Your guess was correct!"
+
