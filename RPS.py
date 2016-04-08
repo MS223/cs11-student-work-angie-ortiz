5d2e8c3c9e0c8fb959c58df6f82667e41f81b8f1
angie-ortiz <angie.ortiz@ms223.org>
2016-04-08 08:34:34 -0400

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,13 +1,16 @@
 import random
-list = ['rock', 'paper', 'scissors']
-choice = random.choice(list)
+list = ['rock', 'paper', 'scissors'] #the game is meant for a user and computer to pick either r,p,s so I knew I had to make a list
+choice = random.choice(list) #the random item should be chosen from the list
 print "Rock, paper, scissors say shoot"
 user= raw_input("User: ")
 print "Computer: " + choice
-while user == choice:
+while user == choice: #if it's a tie, then you go again until someone wins
     print "Tie!"
     print "Rock, paper, scissors say shoot"
     user= raw_input("User: ")
     choice = random.choice(list)
     print "Computer: " + choice
-    #I need an else or elif
+if user == "rock" and choice == "scissor" or user == "paper" and choice == "rock" or user == "scissors" and choice == "paper": #this helps determine when the user wins
+    print "User wins"
+else: #this helps determine when the user loses
+    print "Computer wins"
