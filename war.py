3b1e580ef665eefe3ec6af023f3a62877b79158c
angie-ortiz <angie.ortiz@ms223.org>
2016-04-15 08:39:58 -0400

    Update war.py

--- war.py
+++ war.py
@@ -1,31 +1,38 @@
 import random
-score = 0
+p1_score = 0
+p2_score= 0
 
 player_one= raw_input("Player One's Name: ")
 player_two= raw_input("Player Two's Name: ")
 # shuffled_deck: will return a shuffled deck to the user
-#input:
 #output: a list representing a shuffled deck
 def shuffled_deck():
-    basic_deck = range(2, 54)
+    basic_deck = range(2, 15)* 4
     random.shuffle(basic_deck)
     return basic_deck
 deck= shuffled_deck()
 def player_turn(player_name):
     card= deck.pop()
-    print player_name, 'drew card', card
-    return (card)
+    print player_name, 'drew card', str(card)
+    return str(card)
 
-while len(deck) > 0:
+while len(deck) > 0: #this can only work if there are more than 0 elements
     print len(deck)
     card= player_turn(player_one)
     card1=player_turn(player_two)
     if card > card1:
         print "Player One Wins!"
+        p1_score= p1_score + 1
+
     elif card < card1:
         print "Player Two Wins!"
+        p2_score= p2_score + 1
+
+    else:
+        print "Tie!"
+
+print "Player One's Score: " + str(p1_score)
+print "Player Two's Score: " + str(p2_score)
 
 
-# compare_scores= deck.pop()
-# while player_turn(player_one) or player_turn(player_two):
-#
+
