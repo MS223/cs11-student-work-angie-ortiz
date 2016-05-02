0188524debba11efa68c5eb126f9352f3b4071ff
angie-ortiz <angie.ortiz@ms223.org>
2016-05-02 08:44:36 -0400

    Create may2quiz.py

---
+++ may2quiz.py
@@ -0,0 +1,29 @@
+# integer= input("Give me an integer:")
+# max_value= range(1,integer +1)
+# print max_value
+
+list1 = [4,5,15,11,23,42]
+list2 = [1,8,7,16,7,35]
+def compare_lists():
+    a== str(list1).split
+    b== str(list2).split
+    print list1
+          #positive_integers=[]
+#     if n == str(positive_integers).split:
+#         print sum(positive_integers)
+# digit_sum(1234)
+
+
+
+
+
+
+
+
+
+
+
+# swapping_stars= input("How wide?")
+# height= input("How tall?")
+# for x in range(0,height):
+#     print swapping_stars * "* -"
