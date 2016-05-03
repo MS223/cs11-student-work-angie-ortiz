2a7850fb840eb7e8e080806fe02d555a497cff70
angie-ortiz <angie.ortiz@ms223.org>
2016-05-03 09:13:40 -0400

    Update may2quiz.py

--- may2quiz.py
+++ may2quiz.py
@@ -2,25 +2,16 @@
 # max_value= range(1,integer +1)
 # print max_value
 
-list1 = [4,5,15,11,23,42]
-list2 = [1,8,7,16,7,35]
-def compare_lists():
-    a== str(list1).split
-    b== str(list2).split
-    print list1
-          #positive_integers=[]
-#     if n == str(positive_integers).split:
-#         print sum(positive_integers)
-# digit_sum(1234)
-
-
-
-
-
-
-
-
-
+# list1 = [4,5,15,11,23,42]
+# list2 = [1,8,7,16,7,35]
+# def compare_lists(list1,list2):
+#     for x in list1:
+#         if x> list2[list1.index(x)]:
+#             print x
+#         else:
+#             print list2[list1.index(x)]
+#
+# compare_lists(list1,list2)
 
 
 # swapping_stars= input("How wide?")
