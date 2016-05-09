c593b9d19b04e07c736570c4d2d95a2130d1cbcc
angie-ortiz <angie.ortiz@ms223.org>
2016-05-09 08:42:59 -0400

    Update getting_loopy.py

--- getting_loopy.py
+++ getting_loopy.py
@@ -1,9 +1,9 @@
 fruit= raw_input("Give me a fruit: ")
-def fruit_pluralizer(list_of_strings):
-    for x in fruit:
-        if x[len(x)-1] + "ies":
+def fruit_pluralizer(x): #removed the for loop and replaced the initial parameter in fruit_pluralizer to x
+    # for x in fruit:
+        if x[len(x)-1] == "y":
             print x[:len(x)-1] + "ies"
-    else:
-        print x[:len(x)] + "s"
-
+        else:
+            print x[:len(x)] + "s"
+fruit_pluralizer(fruit)
 #I have to call it to check if it works
