92f59fb45703b0b911bdcf3842d6165dd01245b1
angie-ortiz <angie.ortiz@ms223.org>
2016-05-09 08:42:01 -0400

    Create May 6 Do Now_String Slicing.py

---
+++ May 6 Do Now_String Slicing.py
@@ -0,0 +1,3 @@
+print "hello"[1:] #this is slicing the string
+print "hello"[:4] #the # after the colen represents what index to remove, in this case it removed o
+print "hello"[2:4] #the first # removes the elements indicated
