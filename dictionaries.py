6ea2f640fa542b8a9d0d2ebf06c7939d61fc7251
angie-ortiz <angie.ortiz@ms223.org>
2016-05-16 08:36:01 -0400

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+user_profile= {
+'name': 'Angie',
+'school': 'Laboratory School of Finance and Technology',
+'birthday': 'September 28, 1999',
+'email': 'angie.ortiz@ms223.org',
+'username': 'bigpapi_',
+'where are you from?': 'Honduras',
+'number of siblings': '0',
+'skills': 'Problem Solving,Creative Thinking',
+'about you': 'I love to eat,sleep',
+'favorite quote': 'Live as if you were to die tomorrow',
+'favorite movie': 'Iron Man',
+'favorite musician': 'Lukas Graham',
+'favorite TV show': 'The Vampire Diaries',
+'favorite book': 'The Odyssey',
+}
+
+print user_profile
