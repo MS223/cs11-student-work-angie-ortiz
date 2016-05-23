84862282a3a5142dcd2d0be7950ee9acc206ff81
angie-ortiz <angie.ortiz@ms223.org>
2016-05-23 08:42:12 -0400

    Update ToDo.py

--- ToDo.py
+++ ToDo.py
@@ -1,5 +1,6 @@
 action= raw_input("What would you like to do? ")
 day= raw_input("What day? ").capitalize()
+
 days_of_week= {
 'Monday': [],
 'Tuesday':[],
@@ -10,12 +11,40 @@ days_of_week= {
 'Sunday':[],
 }
 
-def add():
-    days_of_week[day].append(action)
 
+def choice():
+    user_choice= raw_input("How may I help you?" )
+    if user_choice == "add":
+action= raw_input("What would you like to do? ")
+day= raw_input("What day? ").capitalize()
+print add(action,day)
+else:
+print get(day)
+
+def add(action, day):
+    days_of_week[day].append(action)
+#i need an option to call choice
+add(action, day)
+
+# print days_of_week
+
+def get(day):
+    print days_of_week[day]
+
+get(day)
 
 print days_of_week
+#i need an option to call choice
 
+
+def choice():
+    user_choice= raw_input("How may I help you?" )
+    if user_choice == add:
+        print days_of_week
+    else:
+        print get(day)
+    # if they chose add call add
+    #if the choose get call get
 #not adding anything
 
     #days_of_week[day]=action
