3bda4120572179fd47aa5e7da152c7f65f65123c
angie-ortiz <angie.ortiz@ms223.org>
2016-05-24 08:36:21 -0400

    Update ToDo.py

--- ToDo.py
+++ ToDo.py
@@ -1,6 +1,3 @@
-action= raw_input("What would you like to do? ")
-day= raw_input("What day? ").capitalize()
-
 days_of_week= {
 'Monday': [],
 'Tuesday':[],
@@ -11,45 +8,34 @@ days_of_week= {
 'Sunday':[],
 }
 
-
-def choice():
-    user_choice= raw_input("How may I help you?" )
-    if user_choice == "add":
-action= raw_input("What would you like to do? ")
-day= raw_input("What day? ").capitalize()
-print add(action,day)
-else:
-print get(day)
-
-def add(action, day):
-    days_of_week[day].append(action)
-#i need an option to call choice
-add(action, day)
-
-# print days_of_week
-
 def get(day):
     print days_of_week[day]
 
-get(day)
 
-print days_of_week
-#i need an option to call choice
+def add(action, day):
+    days_of_week[day].append(action)
 
 
 def choice():
-    user_choice= raw_input("How may I help you?" )
-    if user_choice == add:
-        print days_of_week
-    else:
-        print get(day)
-    # if they chose add call add
-    #if the choose get call get
-#not adding anything
+    user_choice= "" #had to make them local variables
+    action= ""
+    day= ""
+    while user_choice != "exit": #once I type exit it will stop
+        user_choice= raw_input("How may I help you?" )
+        if user_choice == "add":
+            action= raw_input("What would you like to do? ")
+            day= raw_input("What day? ").capitalize()
+            add(action,day)
+        elif user_choice == "get":
+            day= raw_input("What day? ").capitalize()
+            get(day)
 
-    #days_of_week[day]=action
+
+choice()
 
 
-    #should get our action variable and add it to our dictionary with the key day
+
+
+
 
 
